c05f936f1725c08878031684e3e3fdf8ece22759
Qambitions <63689257+Qambitions@users.noreply.github.com>
2020-11-02 13:00:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 
 using namespace std;
 
-string operatorr[] = {"+" , "-" , "*" , "/" , "^"};
-
-int check_in_operator(string x)
+string operatorr[] = {"+" , "-" , "*" , "/" , "^"}; //operator
+string ERRORR = "E";
+int check_in_operator(string x) //dinh do uu tien
 {
     if (x=="(" || x==")") return -1;
     for (int i=0; i < 5; i++)
@@ -22,7 +22,7 @@ int check_in_operator(string x)
     return 0;
 }
 
-void caculate_balan(vector<string> &v)
+void caculate_balan(vector<string> &v) //thuc hien phep tinh
 {
      string x = v[v.size()-1];
      v.pop_back();
@@ -33,7 +33,12 @@ void caculate_balan(vector<string> &v)
      if (x=="+") a+=b;
      else if (x=="-") a-=b;
      else if (x=="*") a*=b;
-      else if (x=="/") a/=b;
+      else if (x=="/")
+      {
+           if (b==0) // div / 0
+               throw "Math Error";
+           a/=b;
+      }
       else if (x=="^") a = pow(a,b);
      v.push_back(to_string(a));
 }
@@ -69,38 +74,108 @@ void ba_lan(stack< ci > &st, vector<string> &v, string x, int priority,int mode)
     }
 }
 
-
-int main(int arsc, char *arsv[])
+void do_balan(int n_inp, string s_inp[], int mode) // ham main de tinh tuong lenh
 {
-    freopen("inp.inp","r",stdin);
-   // freopen("out.out","w",stdin);
-    //while (cin >> )
-    int n_inp;
-    string s_inp[100];
-    cin >> n_inp;
-    for (int i=0; i<n_inp; i++)
-        cin >> s_inp[i];
-
-
-    //------------------------------------------------
-    stack < ci > st;
+     stack < ci > st;
     st.push(ci("(",-1));
-    int mode=0;
     vector <string> v;
     for (int i=0; i<n_inp; i++)
         if (s_inp[i] == "{" || s_inp[i] == "[") s_inp[i] = "(";
         else if (s_inp[i] == "}" || s_inp[i] == "]") s_inp[i] = ")";
 
-    for (int i=0; i<n_inp; i++)
+     try{
+    for (int i=0; i<n_inp; i++)                           // run ba lan
     {
         int priority = check_in_operator(s_inp[i]);
         if (priority == 0) v.push_back(s_inp[i]);
-        else ba_lan(st,v,s_inp[i],priority,0);
+        else ba_lan(st,v,s_inp[i],priority,mode);
 
     }
-    ba_lan(st,v,")",-1,1);
+
+    ba_lan(st,v,")",-1,mode);
 
     for (int i=0; i<v.size(); i++)
         cout << v[i] << " ";
+     }
+     catch (string error) // catch error
+     {
+          cout << "Math Error \n";
+     }
+}
+
+void make_arr(string tmp, string arr[], int &n_arr) throw(string)// dinh dang default va bat loi
+{
+     //getline(cin,tmp);
+     stringstream stream;
+     stream << tmp[0];
+     for (int i=1; i<tmp.size(); i++)
+          if (tmp[i]=='.')stream << tmp[i];
+          else
+          if (48 <= (int)tmp[i] && (int)tmp[i] <= 57 &&
+               (48 <= (int)tmp[i-1] && (int)tmp[i-1] <= 57 || tmp[i-1]=='-' || tmp[i-1]=='.'))
+                    stream << tmp[i];
+          else
+          {
+               if (!(48 <= (int)tmp[i] && (int)tmp[i] <= 57) && tmp[i]!=' ')
+               {
+                    string x = "";
+                    x = x + tmp[i];
+                    if (check_in_operator(x)==0) //bat loi ki tu la
+                    throw ERRORR;
+               }
+
+               stream << " " << tmp[i];
+          }
+     while (stream >> arr[n_arr++]){}
+     for (int i=0; i<n_arr-1; i++)
+     {
+          if ((check_in_operator(arr[i]) > 0 && check_in_operator(arr[i+1]) > 0))  //bat loi 2 dau lien tuc
+               throw ERRORR;
+     }
+
+}
+
+int main(int arsc, char *arsv[])
+{
+     // prepare from cmd
+     freopen(arsv[1],"r",stdin);
+     freopen(arsv[4],"w",stdout);
+     int n;
+     try{
+          n = atoi(arsv[2]);
+     }
+     catch (invalid_argument &e){
+          cout << "Error cmd";
+          return 0;
+     }
+
+     int act;
+     if (arsv[3] == "-t") act = 0;
+     else if (arsv[3] == "-c") act = 1;
+     else {
+          cout << "Error cmd";
+          return 0;
+     }
+    //------------------------------------------------
+    // time to do
+    //int n=1,act=0;
+    freopen("inp.inp","r",stdin);
+     for (int i=0; i<n; i++)
+     {
+          string tmp;
+          getline(cin , tmp);
+          string arr[100];
+          int n_arr=0;
+          try{
+
+                make_arr(tmp,arr,n_arr);//dinh dang input
+
+               do_balan(n_arr,arr,act);
+          }
+         // catch(invalid_argument &e) {cout << "E \n";}  // catch error
+          catch (const string& e) {cout << "E"<< '\n';}
+          //catch (string &e) {cout << "E \n";}
+     }
     return 0;
 }
+
